208d7eb670a004206b4995db8fa22078c3906dc2
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-01 15:23:17 -0300

    creo servicio

--- src/main/java/com/app/web/servicio/EstudianteServicioImpl.java
+++ src/main/java/com/app/web/servicio/EstudianteServicioImpl.java
@@ -1,4 +1,14 @@
 package com.app.web.servicio;
 
-public class EstudianteServicioImpl {
+import com.app.web.entidad.Estudiante;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EstudianteServicioImpl implements EstudianteServicio {
+
+    @Override
+    public List<Estudiante> listarTodosLosEstudiantes() {
+        return null;
+    }
 }
